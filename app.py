66a463a509564e5abc501371fb0ef2f0581b1d90
Rijo George <rijothedeveloper@gmail.com>
2021-12-06 15:51:57 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
